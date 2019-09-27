d60721ce826c17c31c82e036b4e883839b5a49dc
João Lenon <lenonSec7@hotmail.com>
2019-09-26 22:10:22 -0300

    v0.1

---
+++ solutions/target/test-classes/dataset/departamento.sql
@@ -0,0 +1,2 @@
+INSERT INTO departamento (id, created, nome, descricao)
+VALEUS (1001, now(), 'Desenvolvimento', 'Departamento de desenvolvimento pica das galáxias da flash cursos mermaunnnn')
---
+++ solutions/target/test-classes/dataset/usuarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuario (id, created, email, senha, tipousuario, disponivel)
+VALUES
+(1000, now(), 'jlenon7@hotmail.com', '12345', 0, true),
+(1001, now(), 'lenonsec7@gmail.com', '123456', 2, true);
