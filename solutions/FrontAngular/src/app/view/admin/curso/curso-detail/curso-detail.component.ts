7dce121f789c69834ca8d649b0c03a82a7e6b583
vazio <vazio>
2019-12-03 19:29:11 -0300

    feat: Curso scream

---
+++ solutions/FrontAngular/src/app/view/admin/curso/curso-detail/curso-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso-detail',
+  templateUrl: './curso-detail.component.html',
+  styleUrls: ['./curso-detail.component.css']
+})
+export class CursoDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
